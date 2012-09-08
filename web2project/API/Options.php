8fed55c8ce82b1d6a82f1bc4710faa918c2ab146
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-08 16:19:57 -0500

    fleshed out a basic structure on how OPTIONS might work for web2project

--- web2project/API/Options.php
+++ web2project/API/Options.php
@@ -10,6 +10,53 @@ class web2project_API_Options extends web2project_API_Base {
     
     public function process()
     {
+        $class = new stdClass();
+        $class->resource = '/'.$this->module .'/';
         
+        $class->actions = array(
+            'index'  => array('href'        => $class->resource, 'method' => 'GET'),
+            'filter' => array('href'        => $class->resource, 
+                              'method'      => 'GET',
+                              'optional'    => array('page', 'limit', 'offset')),
+            'search' => array('href'        => $class->resource, 
+                              'method'      => 'GET',
+                              'required'    => array('search')),
+            'view'   => array('href'        => $class->resource, 
+                              'method'      => 'GET',
+                              'required'    => array($this->key)),
+            'create' => array('href'        => $class->resource, 
+                              'method'      => 'POST',
+                              'required'    => $this->getRequired(),
+                              'optional'    => $this->getOptional()),
+            'update' => array('href'        => $class->resource, 
+                              'method'      => 'PATCH',
+                              'required'    => $this->key,
+//TODO: This next getOptional isn't quite correct.. we should remove $this->key
+                              'optional'    => $this->getOptional()),  
+            'delete' => array('href'        => $class->resource,
+                              'method'      => 'DELETE',
+                              'required'    => $this->key),
+
+        );
+
+        $this->app->response()->body(json_encode($class));
+
+        return $this->app;
+    }
+
+    protected function getRequired()
+    {
+        $this->obj->isValid();
+
+        return array_keys($this->obj->getError());
+    }
+
+    protected function getOptional()
+    {        
+        $fields = get_class_vars($this->classname);
+        $this->obj->isValid();
+        $optional = array_diff($fields, $this->obj->getError());
+
+        return array_keys($optional);
     }
 }
