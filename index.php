3b479b14ddc484f3f5028f6eda63835574941a25
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-08 15:08:46 -0500

    updating the main index to handle PATCH and OPTIONS

--- index.php
+++ index.php
@@ -22,19 +22,30 @@ $app->post('/:module', function($module) use ($app) {
     $app = $action->process();
 });
 
-$app->put('/:module/:id', function($module, $id) use ($app) {
-    $action = new web2project_API_Put($app, $module, $id);
-    $app = $action->process();
-});
-
 $app->delete('/:module/:id', function($module, $id) use ($app) {
     $action = new web2project_API_Delete($app, $module, $id);
     $app = $action->process();
 });
 
-$app->options('/:module', function($module) {
-    $action = new web2project_API_Option($app, $module);
+$app->options('/:module', function($module) use ($app) {
+    $action = new web2project_API_Options($app, $module);
     $app = $action->process();
 });
+
+/*
+ * NOTE: 
+ *    Generally, a PUT is supposed to accept the *entire* resource including the
+ *    unchanged fields while a PATCH should effectively be a "diff." For the time
+ *    being, they're both implemented as PATCH. Get over it.
+ */
+$app->put('/:module/:id', function($module, $id) use ($app) {
+    $action = new web2project_API_Put($app, $module, $id);
+    $app = $action->process();
+});
+$app->put('/:module/:id', function($module, $id) use ($app) {
+    $action = new web2project_API_Put($app, $module, $id);
+    $app = $action->process();
+});
+
 
 $app->run();
