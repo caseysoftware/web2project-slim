b04faa57c4d70b836bad8f991e5bd7a14fd9cb6e
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-08 16:17:10 -0500

    formatting tweaks to the base class

--- web2project/API/Base.php
+++ web2project/API/Base.php
@@ -2,27 +2,27 @@
 
 abstract class web2project_API_Base {
 
-    protected $app      = null;
-    protected $module   = null;
-    protected $more     = 0;
+    protected $app       = null;
+    protected $module    = null;
 
-    protected $id       = 0;
-    protected $params   = array();
+    protected $id        = 0;
+    protected $params    = array();
 
-    protected $key      = '';
-    protected $obj      = '';
+    protected $key       = '';
+    protected $obj       = '';
+    protected $classname = '';
 
     public function __construct(Slim $app, $module, $id = 0)
     {
-        $this->app      = $app;
-        $this->module   = $module;
-        $this->key      = unPluralize($this->module).'_id';
+        $this->app       = $app;
+        $this->module    = $module;
+        $this->key       = unPluralize($this->module).'_id';
 
-        $classname      = getClassName($this->module);
-        $this->obj      = new $classname;
+        $this->classname = getClassName($this->module);
+        $this->obj       = new $this->classname;
 
-        $this->id       = $id;
-        $this->params   = $app->request()->params();
+        $this->id        = $id;
+        $this->params    = $app->request()->params();
     }
 
     abstract public function process();
