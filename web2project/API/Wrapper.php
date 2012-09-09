9536ad14864714b88a36330515116a9c15e300a2
D Keith Casey Jr <keith@blueparabola.com>
2012-09-09 03:06:36 -0500

    wired the super resources to be auto-generated based on naming conventions;

--- web2project/API/Wrapper.php
+++ web2project/API/Wrapper.php
@@ -1,53 +1,60 @@
 <?php
 
 class web2project_API_Wrapper {
-    public $object = null;
-    public $classname = '';
+    public $object      = null;
+    public $classname   = '';
+
+    protected $AppUI    = null;
 
     protected $supraresources = array();
     protected $subresources = array();
 
     public function __construct($object) {
-        $this->object = $object;
+        $this->object    = $object;
         $this->classname = get_class($this->object);
+        $this->AppUI     = new w2p_Core_CAppUI();
 
+        $this->object->super_resources = array();
         $this->setSuperResources();
-        $this->setSubResources();
+        $this->object->sub_resources = array();
+//        $this->setSubResources();
     }
 
     public function getObjectExport()
     {
-//TODO: figure out how to auto-determine the sub-resources and/or super resources
-        $supra = array_flip($this->supraresources);
-        $sub = array_flip($this->subresources);
-        $this->object->links = (array_keys($supra + $sub));
-
         return json_encode($this->object);
     }
-
+    
     /*
      * It's easy to know which modules this module/object is dependent on.
      */
     protected function setSuperResources()
     {
-//These are the supraresources for projects
-        $this->supraresources[] = 'companies';
-        $this->supraresources[] = 'users';
+        $modules = $this->AppUI->getActiveModules();
+        foreach($modules as $name => $value) {
+            unset($modules[$name]);
+            $modules[unPluralize($name)] = $name;
+        }
+
+        $fields  = get_class_vars($this->classname);
+        foreach($fields as $field => $value) {
+            $id = $this->object->{$field};
+            $last_underscore = strrpos($field, '_') + 1;
+            $suffix = ($last_underscore !== false) ? substr($field, $last_underscore) : $field;
+
+            if(isset($modules[$suffix]) && $id) {
+                $this->object->super_resources[$modules[$suffix]] = "/{$modules[$suffix]}/".$id;
+            }
+        }
     }
 
     /*
-     * .. but how do we figure out which modules/objects are dependent on it?
-     * 
+     * TODO: .. but how do we figure out which modules/objects are dependent on it?
+     *
      * Do we need a hook_register?
      */
     protected function setSubResources()
     {
-//These are the subresources for projects
-        $this->subresources[] = 'tasks';
-        $this->subresources[] = 'contacts';
-        $this->subresources[] = 'events';
-        $this->subresources[] = 'files';
-        $this->subresources[] = 'links';
-        $this->subresources[] = 'forums';
+        
     }
 }
