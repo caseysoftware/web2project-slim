8292078760a69fe6f5b39f50e818983ba102860a
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-14 09:07:07 -0400

    renaming because this is more applicable

---
+++ web2project/API/Common.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class web2project_API_Common {
+
+    protected $app       = null;
+    protected $module    = null;
+
+    protected $id        = 0;
+    protected $params    = array();
+
+    protected $key       = '';
+    protected $obj       = '';
+    protected $classname = '';
+
+    protected $resources = array();
+
+    public function __construct(Slim $app, $module, $id = 0)
+    {
+        $this->app       = $app;
+        $this->module    = $module;
+        $this->key       = unPluralize($this->module).'_id';
+
+        return $this->init();
+    }
+
+    protected function init()
+    {
+        $AppUI = new w2p_Core_CAppUI();
+        $this->resources = $AppUI->getActiveModules();
+
+        if(isset($this->resources[$this->module])) {
+            $this->classname = getClassName($this->module);
+            $this->obj       = new $this->classname;
+
+            $this->id        = $id;
+            $this->params    = $app->request()->params();
+        } else {
+            $this->app->response()->status(404);
+        }
+
+        return $this->app;
+    }
+
+    abstract public function process();
+}
