51cf71d603b9dd06ca979008e209173420ff7b8a
D Keith Casey Jr <keith@caseysoftware.com>
2012-09-14 22:19:05 -0400

    filtered non-API-able modules

--- web2project/API/Root.php
+++ web2project/API/Root.php
@@ -26,11 +26,12 @@ class web2project_API_Root {
 
         $modules = $this->AppUI->getActiveModules();
 //TODO: There are some non-API-able modules in this list. We need to figure out how to remove them.
+$modules = array_diff($modules, array('History', 'ProjectDesigner', 'Reports', 'SmartSearch', 'System Admin'));
+
         foreach($modules as $path => $module) {
             $subresources[$path] = array('name' => $module,
                 'uri' => $this->output->self.$path);
         }
-        
         $this->output->sub_resources = $subresources;
 
         $api = new web2project_API_Wrapper();
